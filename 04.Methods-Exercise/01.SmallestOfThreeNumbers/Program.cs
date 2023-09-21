a5f00a06f0f6115b10d69acaa3330b91126dc537
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-21 20:01:29 +0300

    Commit first exercise

---
+++ 04.Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
