8fd78328076596af4b8021249884a9ce8c3d51f8
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-21 19:40:04 +0300

    First repo added

---
+++ 05.Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+List<int> wagons = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] input = command.Split().ToArray();
+
+    if (input.Length > 1)
+    {
+        wagons.Add(int.Parse(input[1]));
+    }
+    else if (input.Length <= 1)
+    {
+        for (int i = 0; i < wagons.Count; i++)
+        {
+            if (wagons[i] + int.Parse(input[0]) <= maxCapacityOfWagon)
+            {
+                wagons[i] += int.Parse(input[0]);
+                break;
+            }
+        }
+    }
+}
+
+if (command == "end")
+{
+    Console.WriteLine(string.Join(" ", wagons));
+}
