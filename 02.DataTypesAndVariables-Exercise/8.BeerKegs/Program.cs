1a9133236c8cc49e38b56aa34e6ef8481903a057
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-22 16:07:06 +0300

    Commit solution for ex.08

---
+++ 02.DataTypesAndVariables-Exercise/8.BeerKegs/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int kegsCnt = int.Parse(Console.ReadLine());
+
+double biggestKegVolume = double.MinValue;
+string biggestKegModel = string.Empty;
+
+for (int i = 0; i < kegsCnt; i++)
+{
+    string currKegegModel = Console.ReadLine();
+    double currKegegRadius = double.Parse(Console.ReadLine());
+    double currKegHeight = double.Parse(Console.ReadLine());
+
+    // π * r^2 * h
+    double currKegVolume = Math.PI * Math.Pow(currKegegRadius, 2) * currKegHeight;
+
+    if (currKegVolume > biggestKegVolume)
+    {
+        biggestKegVolume = currKegVolume;
+        biggestKegModel = currKegegModel;
+    }
+}
+
+Console.WriteLine(biggestKegModel);
