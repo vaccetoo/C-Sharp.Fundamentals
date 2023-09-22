f99db0269939ccfe302a2fe6f8f4369608fad5c7
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-22 20:14:08 +0300

    Added more exercises

--- 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/07.VendingMachine/Program.cs
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/07.VendingMachine/Program.cs
@@ -1,2 +1,75 @@
 ﻿
+string command = string.Empty;
 
+string endCommand = string.Empty;
+
+double coinsSum = 0;
+
+while ((command = Console.ReadLine()) != "Start")
+{
+    double currCoins = double.Parse(command);
+    
+    double[] vallidCoins = { 0.1, 0.2, 0.5, 1, 2 };
+
+    if (!vallidCoins.Contains(currCoins))
+    {
+        Console.WriteLine($"Cannot accept {currCoins}");
+        continue;
+    }
+
+    coinsSum += currCoins;
+}
+
+if (command == "Start")
+{
+    while ((endCommand = Console.ReadLine()) != "End")
+    {
+        string currProduct = endCommand;
+
+        string[] vallidProducts = { "Nuts", "Water", "Crisps", "Soda", "Coke" };
+
+        if (!vallidProducts.Contains(currProduct))
+        {
+            Console.WriteLine("Invalid product");
+            continue;
+        }
+
+        double currPrice = 0;
+
+        if (currProduct == "Nuts")
+        {
+            currPrice = 2.0;
+        }
+        else if (currProduct == "Water")
+        {
+            currPrice = 0.7;
+        }
+        else if (currProduct == "Crisps")
+        {
+            currPrice = 1.5;
+        }
+        else if (currProduct == "Soda")
+        {
+            currPrice = 0.8;
+        }
+        else if (currProduct == "Coke")
+        {
+            currPrice = 1.0;
+        }
+
+        if (coinsSum >= currPrice)
+        {
+            Console.WriteLine($"Purchased {currProduct.ToLower()}");
+            coinsSum -= currPrice;
+        }
+        else
+        {
+            Console.WriteLine("Sorry, not enough money");
+        }
+    }
+
+    if (endCommand == "End")
+    {
+        Console.WriteLine($"Change: {coinsSum:f2}");
+    }
+}
---
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,25 @@
+﻿double amountOfMoney = double.Parse(Console.ReadLine());
+int studentsCnt = int.Parse(Console.ReadLine());
+
+double lightsabersPrice = double.Parse(Console.ReadLine());
+double robesPrice = double.Parse(Console.ReadLine());
+double beltsPrice = double.Parse(Console.ReadLine());
+
+double finalPriceForLightsabers = (Math.Ceiling(studentsCnt + 0.1 * studentsCnt)) * lightsabersPrice;
+double finalRobesPrice = studentsCnt * robesPrice;
+
+double freeBelts = studentsCnt / 6;
+double finalBeltsPrice = (studentsCnt - freeBelts) * beltsPrice;
+
+double finalPrice = finalPriceForLightsabers + finalRobesPrice + finalBeltsPrice;
+
+double moneyNeeded = finalPrice - amountOfMoney;
+
+if (finalPrice > amountOfMoney)
+{
+    Console.WriteLine($"John will need {moneyNeeded:f2}lv more.");
+}
+else
+{
+    Console.WriteLine($"The money is enough - it would cost {finalPrice:f2}lv.");
+}
