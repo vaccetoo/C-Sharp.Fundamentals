d43a15e9dd4408c7256f367a18de2fcfc1f65d31
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-22 18:15:59 +0300

    Added exercises from 1 to 7. Exercise 7 is just added having no solution.

---
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/05.Login/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string username = Console.ReadLine();
+
+string userPassword = string.Empty;
+
+for (int i = username.Length - 1; i >= 0; i--)
+{
+    userPassword += username[i];
+}
+
+string currPassword = string.Empty;
+
+int counter = 0;
+
+while ((currPassword = Console.ReadLine()) != userPassword)
+{
+    counter++;
+
+    if (counter >= 4)
+    {
+        Console.WriteLine($"User {username} blocked!");
+        break;
+    }
+
+    Console.WriteLine("Incorrect password. Try again.");
+}
+
+if (currPassword == userPassword)
+{
+    Console.WriteLine($"User {username} logged in.");
+}
---
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+// A number is strong, if the sum of the factorials of each digit is equal to the number itself. 
+
+int numberToCompare = n;
+int allFactorialSum = 0;
+
+while (n > 0)
+{
+    int currDigit = n % 10;
+    n = n / 10;
+
+    int currDigitFactorial = 1;
+
+    for (int i = 1; i <= currDigit; i++)
+    {
+        currDigitFactorial *= i;
+    }
+
+    allFactorialSum += currDigitFactorial;
+}
+
+if (allFactorialSum == numberToCompare)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
