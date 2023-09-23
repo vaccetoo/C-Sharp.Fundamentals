717c4c67654c87c5291cbd9329468ae9820f41e3
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-23 08:43:38 +0300

    Added ex.10 and 11 solutions in 01.BasicSyntax...

--- 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/10.RageExpenses/Program.cs
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/10.RageExpenses/Program.cs
@@ -1,1 +1,41 @@
 ﻿
+int lostGamesCnt = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+int headsetsCnt = 0;
+int mousesCnt = 0;
+int keyboardsCnt = 0;
+int displaysCnt = 0;
+
+for (int i = 1; i <= lostGamesCnt; i++)
+{
+    if (i % 2 == 0)
+    {
+        headsetsCnt++;
+    }
+    if (i % 3 == 0)
+    {
+        mousesCnt++;
+    }
+    if(i % 6 == 0)
+    {
+        keyboardsCnt++;
+
+        if (keyboardsCnt % 2 == 0)
+        {
+            displaysCnt++;
+        }
+    }
+}
+
+double finalHeadsetPrice = headsetsCnt * headsetPrice;
+double finalMousesPrice = mousesCnt * mousePrice;
+double finalKeyboardsPrice = keyboardsCnt * keyboardPrice;
+double finalDisplayPrice = displaysCnt * displayPrice;
+
+double totalPrice = finalHeadsetPrice + finalMousesPrice + finalKeyboardsPrice + finalDisplayPrice;
+
+Console.WriteLine($"Rage expenses: {totalPrice:f2} lv.");
---
+++ 01.BasicSyntaxConditionalStatementsAndLoops-Exercise/11.Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Diagnostics;
+
+int ordersCnt = int.Parse(Console.ReadLine());
+
+double pricePerCapsule = 0;
+double daysCnt = 0;
+int capsuleCnt = 0;
+
+double totalPrice = 0;
+
+for (int i = 1; i <= ordersCnt; i++)
+{
+    pricePerCapsule = double.Parse(Console.ReadLine());
+    daysCnt = double.Parse(Console.ReadLine());
+    capsuleCnt = int.Parse(Console.ReadLine());
+
+    double currentPrice = (daysCnt * capsuleCnt) * pricePerCapsule;
+
+    Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+
+    totalPrice += currentPrice;
+}
+
+Console.WriteLine($"Total: ${totalPrice:f2}");
