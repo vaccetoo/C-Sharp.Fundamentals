0d6b00302f9f5ff99e7b0a7ee7ec822230b1a042
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-24 16:06:09 +0300

    Local commit HeartDelivery

--- 06.MidExamPreparation/HeartDelivery/Program.cs
+++ 06.MidExamPreparation/HeartDelivery/Program.cs
@@ -6,11 +6,57 @@ List<int> houses = Console.ReadLine()
 
 string command = string.Empty;
 
+int startIndex = 0;
+int currIndex = 0;
+
+
 while ((command = Console.ReadLine()) != "Love!")
 {
     List<string> jumpCommand = command.Split(" ").ToList();
     
     int jumpLength = int.Parse(jumpCommand[1]);
+
+    if (startIndex + jumpLength > houses.Count - 1)
+    {
+        startIndex = 0;
+        jumpLength = 0;
+    }
+
+    if (houses[startIndex + jumpLength] == 0)
+    {
+        Console.WriteLine($"Place {startIndex + jumpLength} already had Valentine's day.");
+        continue;
+    }
+
+    houses[startIndex + jumpLength] -= 2;
+
+    if (houses[startIndex + jumpLength] == 0)
+    {
+        Console.WriteLine($"Place {startIndex + jumpLength} has Valentine's day.");
+    }
+
+    currIndex = startIndex + jumpLength;
+    startIndex = currIndex;
 }
 
-Console.WriteLine(string.Join(" ", houses));
+Console.WriteLine($"Cupid's last position was {currIndex}.");
+
+if (houses.Sum() == 0)
+{
+    Console.WriteLine("Mission was successful.");
+}
+else
+{
+    int counter = 0;
+
+    for (int i = 0; i < houses.Count; i++)
+    {
+        if (houses[i] != 0)
+        {
+            counter++;
+        }
+    }
+
+    Console.WriteLine($"Cupid has failed {counter} places.");
+}
+  
