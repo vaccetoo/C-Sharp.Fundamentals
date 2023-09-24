bf9e9fc3f141e12f672d0e528988501ccaa320c4
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-24 13:40:14 +0300

    Heart Delivery Commit

---
+++ 06.MidExamPreparation/HeartDelivery/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<int> houses = Console.ReadLine()
+    .Split("@", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Love!")
+{
+    List<string> jumpCommand = command.Split(" ").ToList();
+    
+    int jumpLength = int.Parse(jumpCommand[1]);
+}
+
+Console.WriteLine(string.Join(" ", houses));
---
+++ 06.MidExamPreparation/ShoppingList/Program.cs
@@ -0,0 +1,75 @@
+﻿
+List<string> products = Console.ReadLine()
+    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Go Shopping!")
+{
+    List<string> currCommand = command
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+    if (currCommand.First() == "Urgent")
+    {
+        AddItemAtTheStart(products, currCommand);
+    }
+    else if (currCommand.First() == "Unnecessary")
+    {
+        RemoveTheItem(products, currCommand);
+    }
+    else if (currCommand.First() == "Correct")
+    {
+        SwapTheItems(products, currCommand);
+    }
+    else if (currCommand.First() == "Rearrange")
+    {
+        PlaceTheItemAtTheEnd(products, currCommand);
+    }
+}
+
+Console.WriteLine(string.Join(", ", products));
+
+void PlaceTheItemAtTheEnd(List<string> products, List<string> currCommand)
+{
+    // Rearrange {item}" - if the grocery exists in the list, remove it from its current position and add it at the end of the list. Otherwise, skip this command.
+
+    if (products.Contains(currCommand[1]))
+    {
+        products.Remove(currCommand[1]);
+        products.Add(currCommand[1]);
+    }
+}
+
+void SwapTheItems(List<string> products, List<string> currCommand)
+{
+    // "Correct {oldItem} {newItem}" - if the item with the given old name exists, change its name with the new one. Otherwise, skip this command
+
+    if (products.Contains(currCommand[1]))
+    {
+        int index = products.IndexOf(currCommand[1]);
+        products.RemoveAt(index);
+        products.Insert(index, currCommand[2]);
+    }
+}
+
+void RemoveTheItem(List<string> products, List<string> currCommand)
+{
+    // "Unnecessary {item}" - remove the item with the given name, only if it exists in the list. Otherwise, skip this command.
+
+    if (products.Contains(currCommand[1]))
+    {
+        products.Remove(currCommand[1]); 
+    }
+}
+
+void AddItemAtTheStart(List<string> products, List<string> currCommand)
+{
+    // "Urgent {item}" - add the item at the start of the list.  If the item already exists, skip this command.
+
+    if (!products.Contains(currCommand[1]))
+    {
+        products.Insert(0, currCommand[1]);
+    }
+}
