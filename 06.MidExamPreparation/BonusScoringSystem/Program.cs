4c3de65fd78f12d108fb27b7847af6e3fe73e45a
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-25 20:42:40 +0300

    Add more exercises

---
+++ 06.MidExamPreparation/BonusScoringSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int studentsCnt = int.Parse(Console.ReadLine());
+int lecturesCnt = int.Parse(Console.ReadLine());
+int addBonus = int.Parse(Console.ReadLine());
+
+// On the following lines, you will be receiving the attendance of each student.
+
+double maxBonusPoints = 0;
+int finalAttendance = 0;
+
+for (int i = 1; i <= studentsCnt; i++)
+{
+    int currStudentAttendance = int.Parse(Console.ReadLine());
+
+    double currStudentBonusPoints = ((double)currStudentAttendance / lecturesCnt) * (5 + addBonus);
+
+    if (currStudentBonusPoints > maxBonusPoints)
+    {
+        maxBonusPoints = currStudentBonusPoints;
+        finalAttendance = currStudentAttendance;
+    }
+}
+
+Console.WriteLine($"Max Bonus: {Math.Ceiling(maxBonusPoints)}.");
+Console.WriteLine($"The student has attended {finalAttendance} lectures.");
+    
