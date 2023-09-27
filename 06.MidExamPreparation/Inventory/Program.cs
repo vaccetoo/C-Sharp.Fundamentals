638336ec3ea7a385b6b541767fe2ba1dc981bfab
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-27 20:30:27 +0300

    Inventory not finished

---
+++ 06.MidExamPreparation/Inventory/Program.cs
@@ -0,0 +1,59 @@
+﻿
+List<string> journal = Console.ReadLine()
+    .Split(", ")
+    .ToList();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Craft!")
+{
+    string[] currCommand = command.Split(" - ");
+
+    string item = currCommand[1];
+
+    if (currCommand.First() == "Collect")
+    {
+        if (!journal.Contains(item))
+        {
+            journal = AddItem(journal, item);
+        }
+    }
+    else if (currCommand.First() == "Drop")
+    {
+        if (journal.Contains(item))
+        {
+            journal.Remove(item);
+        }
+    }
+    else if (currCommand.First() == "Combine")
+    {
+        string oldItem = currCommand[2];
+        string newItem = currCommand[3];
+
+        if (journal.Contains(oldItem))
+        {
+            int index = journal.IndexOf(oldItem);
+            journal.Insert(index + 1, newItem);
+        }
+    }
+    else if (currCommand.First() == "Renew")
+    {
+        if (journal.Contains(item))
+        {
+            journal.Remove(item);
+            journal.Add(item);
+        }
+    }
+}
+
+if (command == "Craft!")
+{
+    Console.WriteLine(string.Join(", ", journal));
+}
+
+List<string> AddItem(List<string> journal, string item)
+{
+    journal.Add(item);
+
+    return journal;
+}
