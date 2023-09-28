c0416568463a9892195146e54f7bd36e4c0f427e
Vasil Delchev <vasildelchev97@gmail.com>
2023-09-28 23:02:25 +0300

    Added CounterStrike

---
+++ 06.MidExamPreparation/Counter-Strike/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int energy = int.Parse(Console.ReadLine());
+int wonBattles = 0;
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "End of battle")
+{
+
+    int distance = int.Parse(command);
+
+    if (energy < distance)
+    {
+        Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+
+        energy -= distance;
+        break;
+    }
+
+    energy -= distance;
+    wonBattles++;
+
+    if (wonBattles % 3 == 0)
+    {
+        energy += wonBattles;
+    }
+}
+
+if (energy >= 0)
+{
+    Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
+}
