e69de2b1bee1f986a9cfdf38d6d61477b6c692d6
Vasil Delchev <vasildelchev97@gmail.com>
2023-10-10 23:22:57 +0300

    Added ShootForTheWin

---
+++ 06.MidExamPreparation/ShootForTheWin/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int[] targets = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string command = string.Empty;
+
+int shotTargets = 0;
+
+while ((command = Console.ReadLine()) != "End")
+{
+    int currTarget = int.Parse(command);
+
+    if (currTarget >= targets.Length)
+    {
+        continue;
+    }
+
+    int currTargetValue = targets[currTarget];
+
+    if (targets[currTarget] != -1)
+    {
+        targets[currTarget] = -1;
+        shotTargets++;
+    }
+
+    for (int i = 0; i < targets.Length; i++)
+    {
+        if (targets[i] > currTargetValue && targets[i] != -1)
+        {
+            targets[i] -= currTargetValue;
+        }
+        else if (targets[i] <= currTargetValue && targets[i] != -1)
+        {
+            targets[i] += currTargetValue;
+        }
+    }
+}
+
+Console.WriteLine($"Shot targets: {shotTargets} -> {string.Join(" ", targets)}");
