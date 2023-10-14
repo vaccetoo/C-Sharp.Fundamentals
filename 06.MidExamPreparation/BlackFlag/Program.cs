aae63d1e1e3e30b919c1278416a9553527e0f5b0
Vasil Delchev <vasildelchev97@gmail.com>
2023-10-14 11:33:54 +0300

    Added BlackFlag

--- 06.MidExamPreparation/BlackFlag/Program.cs
+++ 06.MidExamPreparation/BlackFlag/Program.cs
@@ -1,1 +1,36 @@
 ﻿
+int days = int.Parse(Console.ReadLine());
+int dailyPlunder =  int.Parse(Console.ReadLine());
+double expectedPlunder = double.Parse(Console.ReadLine());
+
+double totalPlunder = 0;
+double addPlunder = 0;
+double lostPlunder = 0;
+
+for (int i = 1; i <= days; i++)
+{
+    totalPlunder += dailyPlunder;
+
+    if (i % 3 == 0)
+    {
+        addPlunder += 0.5 * dailyPlunder;
+        totalPlunder += 0.5 * dailyPlunder;
+    }
+
+    if (i % 5 == 0)
+    {
+        lostPlunder = 0.3 * totalPlunder;
+        totalPlunder -= lostPlunder;
+    }
+}
+
+double percentPlunder = (totalPlunder / expectedPlunder) * 100;
+
+if (totalPlunder >= expectedPlunder)
+{
+    Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+}
+else
+{
+    Console.WriteLine($"Collected only {percentPlunder:f2}% of the plunder.");
+}
