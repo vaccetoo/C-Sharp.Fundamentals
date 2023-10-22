d2a3d0d05d7278614c9150dbc6f6a7c06fa5a80c
Vasil Delchev <vasildelchev97@gmail.com>
2023-10-22 11:52:40 +0300

    Changed folders

---
+++ RegularMidExam/SpaceTravel/Program.cs
@@ -0,0 +1,75 @@
+﻿
+List<string> route = Console.ReadLine()
+    .Split("||", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+int fuelAmount = int.Parse(Console.ReadLine());
+int amunitionsAmount = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < route.Count; i++)
+{
+    List<string> command = route[i].Split().ToList();
+
+    string currCommand = command[0];
+
+    if (currCommand == "Travel")
+    {
+        int distance = int.Parse(command[1]);
+
+        fuelAmount -= distance;
+
+        if (fuelAmount >= 0)
+        {
+            Console.WriteLine($"The spaceship travelled {distance} light-years.");
+        }
+        else
+        {
+            Console.WriteLine($"Mission failed.");
+
+            break;
+        }
+    }
+    else if (currCommand == "Enemy")
+    {
+        int enemyArmor = int.Parse(command[1]);
+
+        if (amunitionsAmount >= enemyArmor)
+        {
+            amunitionsAmount -= enemyArmor;
+
+            Console.WriteLine($"An enemy with {enemyArmor} armour is defeated.");
+        }
+        else
+        {
+            fuelAmount -= enemyArmor * 2;
+
+            if (fuelAmount >= 0)
+            {
+                Console.WriteLine($"An enemy with {enemyArmor} armour is outmaneuvered.");
+            }
+            else
+            {
+                Console.WriteLine($"Mission failed.");
+
+                break;
+            }
+        }
+    }
+    else if (currCommand == "Repair")
+    {
+        int addFuel = int.Parse(command[1]);
+
+        int addAmmunitions = addFuel * 2;
+
+        fuelAmount += addFuel;
+        amunitionsAmount += addAmmunitions;
+
+        Console.WriteLine($"Ammunitions added: {addAmmunitions}.");
+        Console.WriteLine($"Fuel added: {addFuel}.");
+    }
+    else if (currCommand == "Titan")
+    {
+        Console.WriteLine("You have reached Titan, all passengers are safe.");
+        break;
+    }
+}
---
+++ RegularMidExam/TheAngryCat/Program.cs
@@ -0,0 +1,65 @@
+﻿List<int> items = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int entryPoint = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+int entryItemValue = items[entryPoint];
+
+int leftDmg = 0;
+int rightDmg = 0;
+
+string position = string.Empty;
+
+if (command == "cheap")
+{
+    for (int i = entryPoint - 1; i >= 0; i--)
+    {
+        if (items[i] < entryItemValue)
+        {
+            leftDmg += items[i];
+        }
+    }
+
+    for (int i = entryPoint + 1; i < items.Count; i++)
+    {
+        if (items[i] < entryItemValue)
+        {
+            rightDmg += items[i];
+        }
+    }
+}
+else if (command == "expensive")
+{
+    for (int i = entryPoint - 1; i >= 0; i--)
+    {
+        if (items[i] >= entryItemValue)
+        {
+            leftDmg += items[i];
+        }
+    }
+
+    for (int i = entryPoint + 1; i < items.Count; i++)
+    {
+        if (items[i] >= entryItemValue)
+        {
+            rightDmg += items[i];
+        }
+    }
+}
+
+if (rightDmg > leftDmg)
+{
+    position = "Right";
+
+    Console.WriteLine($"{position} - {rightDmg}");
+}
+else
+{
+    position = "Left";
+
+    Console.WriteLine($"{position} - {leftDmg}");
+}
