a920d4c52d419f3d81cbb518f1b12fff6872ff3f
Vasil Delchev <vasildelchev97@gmail.com>
2023-10-22 11:28:14 +0300

    Added MidExam

--- RegularMidExam/ConsoleApp1/Program.cs
+++ RegularMidExam/ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double budget = double.Parse(Console.ReadLine());
+int studentsCnt = int.Parse(Console.ReadLine());
+double flourPrice = double.Parse(Console.ReadLine());
+double eggPrice = double.Parse(Console.ReadLine());
+double apronPrice = double.Parse(Console.ReadLine());
+
+int freePackages = 0;
+
+for (int i = 1; i <= studentsCnt; i++)
+{
+    if (i % 5 == 0)
+    {
+        freePackages++;
+    }
+}
+
+double finalFlourPrice = (studentsCnt - freePackages) * flourPrice;
+
+double finalEggsPrice = studentsCnt * 10 * eggPrice;
+
+double addAprons = Math.Ceiling(0.2 * studentsCnt);
+double finalApronPrice = (studentsCnt + addAprons) * apronPrice;
+
+double totalPrice = finalFlourPrice + finalEggsPrice + finalApronPrice;
+
+if (totalPrice <= budget)
+{
+    Console.WriteLine($"Items purchased for {totalPrice:f2}$.");
+}
+else
+{
+    Console.WriteLine($"{(totalPrice - budget):f2}$ more needed.");
+}
