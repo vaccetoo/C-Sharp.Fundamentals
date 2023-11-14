6b8daadbb73bd7be6297d2a4dc9e0e50be42f8e7
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-14 20:56:52 +0200

    Added CompanyUsers problem

---
+++ 08.AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string command = string.Empty;
+
+Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] info = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string companyName = info[0];
+    string employeeId = info[1];
+
+    if (!companies.ContainsKey(companyName))
+    {
+        companies.Add(companyName, new List<string>());
+    }
+
+    if (companies[companyName].Contains(employeeId))
+    {
+        continue;
+    }
+
+    companies[companyName].Add(employeeId);
+}
+
+foreach (var company in companies)
+{
+    Console.WriteLine($"{company.Key}");
+
+    foreach (var employeeId in company.Value)
+    {
+        Console.WriteLine($"-- {employeeId}");
+    }
+}
