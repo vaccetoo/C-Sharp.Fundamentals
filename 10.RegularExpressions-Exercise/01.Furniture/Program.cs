36624932b48ddbac1ede46fabffe2976f4237caf
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-19 17:20:16 +0200

    Added 2 Regex problems

---
+++ 10.RegularExpressions-Exercise/01.Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+Regex captureFurnitures = new Regex(@">>(?<name>[A-Za-z]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>\d+)");
+
+string command = String.Empty;
+
+List<Match> matches = new List<Match>();
+
+decimal totalPrice = 0;
+
+while ((command = Console.ReadLine()) != "Purchase")
+{
+    Match match = captureFurnitures.Match(command);
+
+    if (match.Success)
+    {
+        matches.Add(match);
+
+        string name = match.Groups["name"].Value;
+        decimal price = decimal.Parse(match.Groups["price"].Value);
+        int quantity = int.Parse(match.Groups["quantity"].Value);
+
+        decimal currentSum = price * quantity;
+
+        totalPrice += currentSum;
+    }
+}
+
+Console.WriteLine($"Bought furniture:");
+
+foreach (Match match in matches)
+{
+    Console.WriteLine(match.Groups["name"].Value);
+}
+
+Console.WriteLine($"Total money spend: {totalPrice:f2}");
---
+++ 10.RegularExpressions-Exercise/02.Race/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Text;
+using System.Text.RegularExpressions;
+
+List<string> racers = Console.ReadLine()
+    .Split(", ")
+    .ToList();
+
+string command = string.Empty;
+
+Dictionary<string, int> participants = new Dictionary<string, int>();
+
+while ((command = Console.ReadLine()) != "end of race")
+{
+
+    MatchCollection matchedNames = Regex.Matches(command, @"([A-Za-z]+)");
+    MatchCollection matchedDistance = Regex.Matches(command, @"(\d)");
+
+    string name = string.Join("", matchedNames);
+    int distance = matchedDistance.Select(x => int.Parse(x.Value)).Sum();
+
+    if (racers.Contains(name))
+    {
+        if (!participants.ContainsKey(name))
+        {
+            participants.Add(name, distance);
+        }
+        else
+        {
+            participants[name] += distance;
+        }
+    }
+}
+
+int counter = 1;
+
+foreach (var person in participants.OrderByDescending(x => x.Value).Take(3))
+{
+    if (counter == 1)
+    {
+        Console.WriteLine($"1st place: {person.Key}");
+    }
+    else if (counter == 2)
+    {
+        Console.WriteLine($"2nd place: {person.Key}");
+    }
+    else
+    {
+        Console.WriteLine($"3rd place: {person.Key}");
+    }
+
+    counter++;
+}
