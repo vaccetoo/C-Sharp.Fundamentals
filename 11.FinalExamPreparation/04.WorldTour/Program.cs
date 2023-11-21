a970f130c4bf35b4854edde6235e825436ffb694
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-21 22:27:26 +0200

    Added 2 problems

---
+++ 11.FinalExamPreparation/04.WorldTour/Program.cs
@@ -0,0 +1,62 @@
+﻿
+string stops = Console.ReadLine();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Travel")
+{
+    string[] commandInfo = command.Split(":");
+
+    string commandName = commandInfo[0];
+
+    if (commandName == "Add Stop")
+    {
+        int index = int.Parse(commandInfo[1]);
+        string stringToInsert = commandInfo[2];
+
+        if (index < stops.Length)
+        {
+            stops = stops.Insert(index, stringToInsert);
+            Console.WriteLine(stops);
+        }
+        else
+        {
+            Console.WriteLine(stops);
+            continue;
+        }
+    }
+    else if (commandName == "Remove Stop")
+    {
+        int startIndex = int.Parse(commandInfo[1]);
+        int endIndex = int.Parse(commandInfo[2]);
+
+        if (startIndex >= 0 && endIndex < stops.Length)
+        {
+            stops = stops.Remove(startIndex, endIndex - startIndex + 1);
+            Console.WriteLine(stops);
+        }
+        else
+        {
+            Console.WriteLine(stops);
+            continue;
+        }
+    }
+    else if (commandName == "Switch")
+    {
+        string oldString = commandInfo[1];
+        string newString = commandInfo[2];
+
+        if (stops.Contains(oldString))
+        {
+            stops = stops.Replace(oldString, newString);
+            Console.WriteLine(stops);
+        }
+        else
+        {
+            Console.WriteLine(stops);
+            continue;
+        }
+    }
+}
+
+Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
---
+++ 11.FinalExamPreparation/05.DestinationMapper/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+Regex findName = new Regex(@"(\=|\/)((?<name>[A-Z][A-Za-z]{2,}))\1");
+
+MatchCollection matches = findName.Matches(input);
+
+int travelPoints = 0;
+
+List<string> destinations = new List<string>();
+
+foreach (Match match in matches)
+{
+    destinations.Add(match.Groups["name"].Value);
+
+    travelPoints += int.Parse(match.Groups["name"].Length.ToString());
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+Console.WriteLine($"Travel Points: {travelPoints}");
