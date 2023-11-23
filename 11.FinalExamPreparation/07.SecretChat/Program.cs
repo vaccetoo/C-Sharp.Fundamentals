cfb621ed69077902b967cf4d3cf6ec8c24653869
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-23 21:41:46 +0200

    Commit 07.SecretChat

---
+++ 11.FinalExamPreparation/07.SecretChat/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Text;
+
+string message = Console.ReadLine();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Reveal")
+{
+    string[] commandinfo = command.Split(":|:");
+
+    string commandName = commandinfo[0];
+
+    if (commandName == "InsertSpace")
+    {
+        int index = int.Parse(commandinfo[1]);
+
+        message = message.Insert(index, " ");
+
+        Console.WriteLine(message);
+    }
+    else if (commandName == "Reverse")
+    {
+        string substring = commandinfo[1];
+
+        if (message.Contains(substring))
+        {
+
+            int startIndex = message.IndexOf(substring);
+
+            message = message.Remove(startIndex, substring.Length);
+
+            string reversedString = new string(substring.Reverse().ToArray());
+            message += reversedString;
+
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
+    else if (commandName == "ChangeAll")
+    {
+        string substring = commandinfo[1];
+        string replacment = commandinfo[2];
+
+        message = message.Replace(substring, replacment);
+
+        Console.WriteLine(message);
+    }
+}
+
+Console.WriteLine($"You have a new text message: {message}");
