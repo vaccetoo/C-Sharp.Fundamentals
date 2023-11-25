21f121cbf1e8739f9cbe662ff4e6fe2f3359e961
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-25 17:07:34 +0200

    Commit MirrorWords and NeedForSpeedlll

---
+++ 11.FinalExamPreparation/08.MirrorWords/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+Regex findWords = new Regex(@"(@|#)(?<firstWord>[A-z]{3,})\1{2}(?<secondWord>[A-z]{3,})\1");
+
+MatchCollection matches = findWords.Matches(input);
+
+List<string> mirrorWords = new List<string>();
+
+if (matches.Count == 0)
+{
+    Console.WriteLine("No word pairs found!");
+}
+else
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+
+    foreach (Match match in matches)
+    {
+        string firstWord = match.Groups["firstWord"].Value;
+        string secondWord = match.Groups["secondWord"].Value;
+        string reversedSecondWord = new string(match.Groups["secondWord"].Value.Reverse().ToArray());
+
+        if (firstWord == reversedSecondWord)
+        {
+            mirrorWords.Add($"{firstWord} <=> {secondWord}");
+        }
+    }
+}
+
+if (mirrorWords.Count == 0)
+{
+    Console.WriteLine("No mirror words!");
+}
+else
+{
+    Console.WriteLine("The mirror words are:");
+    Console.WriteLine(string.Join(", ", mirrorWords));
+}
---
+++ 11.FinalExamPreparation/09.NeedForSpeedIII/Program.cs
@@ -0,0 +1,122 @@
+﻿int carsCount = int.Parse(Console.ReadLine());
+
+List<Car> cars = new List<Car>();
+
+for (int i = 0; i < carsCount; i++)
+{
+    string[] carsInfo = Console.ReadLine()
+        .Split("|");
+
+    string model = carsInfo[0];
+    int mileage = int.Parse(carsInfo[1]);
+    int fuel = int.Parse(carsInfo[2]);
+
+    Car car = new Car(model, mileage, fuel);
+
+    cars.Add(car);
+}
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Stop")
+{
+    string[] commandInfo = command
+        .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+    string commandName = commandInfo[0];
+
+    if (commandName == "Drive")
+    {
+        string carModel = commandInfo[1];
+        int distance = int.Parse(commandInfo[2]);
+        int fuel = int.Parse(commandInfo[3]);
+
+        foreach (Car car in cars)
+        {
+            if (car.Model == carModel && car.Fuel >= fuel)
+            {
+                car.Mileage += distance;
+                car.Fuel -= fuel;
+
+                Console.WriteLine($"{car.Model} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
+                if (car.Mileage >= 100000)
+                {
+                    Console.WriteLine($"Time to sell the {car.Model}!");
+                }
+            }
+            else if (car.Model == carModel && car.Fuel < fuel)
+            {
+                Console.WriteLine("Not enough fuel to make that ride");
+            }
+        }
+    }
+    else if (commandName == "Refuel")
+    {
+        string carModel = commandInfo[1];
+        int addFuel = int.Parse(commandInfo[2]);
+
+        foreach (Car car in cars)
+        {
+            if (car.Model == carModel)
+            {
+                if (car.Fuel + addFuel > 75)
+                {
+                    addFuel = 75 - car.Fuel;
+                    car.Fuel = 75;
+                }
+                else
+                {
+                    car.Fuel += addFuel;
+                }
+
+                Console.WriteLine($"{car.Model} refueled with {addFuel} liters");
+            }
+        }
+    }
+    else if (commandName == "Revert")
+    {
+        string carModel = commandInfo[1];
+        int kilometers = int.Parse(commandInfo[2]);
+
+        foreach (Car car in cars)
+        {
+            if (car.Model == carModel)
+            {
+                car.Mileage -= kilometers;
+
+                if (car.Mileage >= 10000)
+                {
+                    Console.WriteLine($"{car.Model} mileage decreased by {kilometers} kilometers");
+                }
+                else
+                {
+                    car.Mileage = 10000;
+                }
+            }
+        }
+    }
+}
+
+cars.RemoveAll(car => car.Mileage >= 100000);
+
+foreach (Car car in cars)
+{
+    Console.WriteLine($"{car.Model} -> Mileage: {car.Mileage} kms, Fuel in the tank: {car.Fuel} lt.");
+}
+
+public class Car
+{
+    public Car(string model, int mileage, int fuel)
+    {
+        Model = model;
+        Mileage = mileage;
+        Fuel = fuel;
+    }
+
+    public string Model { get; set; }
+
+    public int Mileage { get; set; }
+
+    public int Fuel { get; set; }
+}
