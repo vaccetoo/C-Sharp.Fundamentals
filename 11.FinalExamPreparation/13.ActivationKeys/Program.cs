b242a0725c389acdf2bdd2a0ca222f39f725c2ac
Vasil Delchev <vasildelchev97@gmail.com>
2023-11-26 15:08:34 +0200

    Commit 13. and 14.

---
+++ 11.FinalExamPreparation/13.ActivationKeys/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+
+string activationKey = Console.ReadLine();
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Generate")
+{
+    string[] commandInfo = command
+        .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+
+    string commandName = commandInfo[0];
+
+    if (commandName == "Contains")
+    {
+        string substring = commandInfo[1];
+
+        if (activationKey.Contains(substring))
+        {
+            Console.WriteLine($"{activationKey} contains {substring}");
+        }
+        else
+        {
+            Console.WriteLine("Substring not found!");
+        }
+    }
+    else if (commandName == "Flip")
+    {
+        string flippCase = commandInfo[1];
+        int startIndex = int.Parse(commandInfo[2]);
+        int endIndex = int.Parse(commandInfo[3]);
+
+        if (flippCase == "Upper")
+        {
+            string substring = activationKey.Substring(startIndex, endIndex - startIndex);
+
+            activationKey = activationKey.Replace(substring, substring.ToUpper());
+        }
+        else if(flippCase == "Lower")
+        {
+            string substring = activationKey.Substring(startIndex, endIndex - startIndex);
+
+            activationKey = activationKey.Replace(substring, substring.ToLower());
+        }
+
+        Console.WriteLine(activationKey);
+    }
+    else if (commandName == "Slice")
+    {
+        int startIndex = int.Parse(commandInfo[1]);
+        int endIndex = int.Parse(commandInfo[2]);
+
+        activationKey = activationKey.Remove(startIndex, endIndex - startIndex);
+        Console.WriteLine(activationKey);
+    }
+}
+
+Console.WriteLine($"Your activation key is: {activationKey}");
---
+++ 11.FinalExamPreparation/14.EmojiDetector/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+Regex regex = new Regex(@"(:{2}|\*{2})(?<emoji>[A-Z][a-z]{2,})\1");
+
+MatchCollection matches = regex.Matches(input);
+
+int coolThresholdSum = 1;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (char.IsDigit(input[i]))
+    {
+        coolThresholdSum *= int.Parse(input[i].ToString());
+    }
+}
+
+Console.WriteLine($"Cool threshold: {coolThresholdSum}");
+Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+
+List<Match> coolestEmojies = new List<Match>();
+
+foreach (Match match in matches)
+{
+    int currentEmojiCoolness = 1;
+
+    string currentEmojiAsString = match.Groups["emoji"].Value;
+
+    for (int i = 0; i < currentEmojiAsString.Length; i++)
+    {
+        currentEmojiCoolness += currentEmojiAsString[i];
+    }
+
+    if (currentEmojiCoolness >= coolThresholdSum)
+    {
+        coolestEmojies.Add(match);
+    }
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, coolestEmojies));
+
